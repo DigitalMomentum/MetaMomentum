86f4fac9fc15e50e468901c647fa8ff50c3aeaf4
David Sheiles <david@digitalmomentum.com.au>
2021-04-15 22:18:43 +1000

    First Release of MetaMomentum

--- MetaMomentum.Umbraco/App_Data/Models/models.generated.cs
+++ MetaMomentum.Umbraco/App_Data/Models/models.generated.cs
@@ -18,7 +18,7 @@ using Umbraco.Web;
 using Umbraco.ModelsBuilder.Embedded;
 
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "27443d47d99da21b")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.4")]
 
 namespace Umbraco.Web.PublishedModels
 {
