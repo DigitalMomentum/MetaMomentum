3986962859ac852cba8f8cc217e602c7bf909ecc
David Sheiles <david@digitalmomentum.com.au>
2025-02-27 09:01:51 +1100

    Remove Angular JS Projects

--- MetaMomentum.UmbracoV10/App_Plugins/MetaMomentum/uui-loader.js
+++
@@ -1,5 +0,0 @@
-﻿if (typeof uui === 'undefined') {
-	var script = document.createElement('script');
-	script.src = "https://cdn.jsdelivr.net/npm/@umbraco-ui/uui@latest/dist/uui.min.js";
-	document.head.appendChild(script);
-}
