a47af6c8f0e1fe264ed183ead9f490009324b67e
David Sheiles <david@digitalmomentum.com.au>
2025-04-14 14:40:59 +1000

    Update build pipeline

--- MetaMomentum/Client/src/entrypoints/manifest.ts
+++
@@ -1,8 +0,0 @@
-export const manifests: Array<UmbExtensionManifest> = [
-  {
-    name: "Meta Momentum Entrypoint",
-    alias: "MetaMomentum.Entrypoint",
-    type: "backofficeEntryPoint",
-    js: () => import("./entrypoint"),
-  }
-];
